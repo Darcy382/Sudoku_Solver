c4169f83f8c59774534d6872b2936bbd364ba56c
Darcy382 <kyledarcy24@icloud.com>
2020-01-30 11:27:58 -0500

    Commit

--- test.py
+++ test.py
@@ -1,7 +1,17 @@
-def this_func():
-    """
+num_possibilities = [[1, (5, 4)], [2], [3], [4], [5], [6], [7], [8], [9]]
 
-    :return: 0
-    """
-    return 0
+row_num = 5
+column_num = 4
+num = [1, 4, 6]
+for possibility in num_possibilities:
+    if possibility[0] in num:
+        possibility.append((row_num, column_num))
 
+for num in num_possibilities:
+    if len(num) == 3:
+        if num[1][0] == num[2][0]:
+            print("Row", num)
+        elif num[1][1] == num[2][1]:
+            print("Column", num)
+
+print(num_possibilities)
